656352cbe368a2c0712ac87209fd2389278a3aa9
Lenny Lorenz <lennylorenz88@gmail.com>
2024-07-22 12:26:30 +0200

    improve structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import LandingPage from './Components/LandingPage';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
+      <LandingPage />
     </div>
   );
 }
