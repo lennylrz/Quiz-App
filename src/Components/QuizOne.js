251b8a982b8c7e3f7993ef6c6ce930984c3e1da2
Lenny Lorenz <lennylorenz88@gmail.com>
2024-07-22 19:49:21 +0200

    add functionality for the geography quiz

--- src/Components/QuizOne.js
+++ src/Components/QuizOne.js
@@ -1,32 +1,84 @@
 import React, { useState } from "react";
 import styles from './Quiz.css'
 function QuizOne() {
+    const [questions, setQuestions] = useState([
+        {
+            question: 'What is the least visited Country in the World?',
+            options: ['Tuvalu', 'San Marino', 'Luxembourg', 'Micronesia', 'China'],
+            correct: 'Tuvalu'
+        },
+        {
+            question: 'In which state can you find the Grand Canyon?',
+            options: ['Nevada', 'California', 'New Mexico', 'Arizona', 'Oregon'],
+            correct: 'Arizona'
+        },
+        {
+            question: 'To the nearest million, how many people live in New Zealand?',
+            options: ['15 million', '25 million', '5 million', '50 million', '40 million'],
+            correct: '5 million'
+        },
+        {
+            question: 'Which country is made up of the most islands?',
+            options: ['Phillipines', 'Indonesia', 'Greece', 'Finland', 'Norway'],
+            correct: 'Norway'
+        },{
+            question: 'Which country has the most pyramids?',
+            options: ['Egypt', 'Sudan', 'Mexico', 'Nigeria', 'Honduras'],
+            correct: 'Sudan'
+        },
+    ])
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const currentQuestion = questions[currentIndex];
+
+    function handleSubmit() {
+        if(selectedAnswer) {
+            setIsSubmitted(true)
+        } else {
+            alert('At least guess Dawg.')
+        }
+    }
+
+    function handleRadioChange(event) {
+        setSelectedAnswer(event.target.value)
+    }
+
+    function nextQuestion() {
+        setIsSubmitted(false);
+        setSelectedAnswer('')
+        if (currentIndex < questions.length - 1){
+        setCurrentIndex((prevIndex) => prevIndex + 1)
+    } else {
+        alert('Im Sorry thats It!')
+    }
+    } 
+
+
     return(
         <div className="quiz">
-            <span className="question-label">What is the Capital of Germany?</span>
+            <span className="question-label">{currentQuestion.question}</span>
             <div className="radio">
-                <div className="singular">
-                <input type="radio" id="One" name="selectanswer"/>
-                <label for='One'>Option 1</label> <br />
-                </div>
-                <div className="singular">
-                <input type="radio" id="Two"  name="selectanswer"/>
-                <label for='Two'>Option 2</label> <br />
-                </div>
-                <div className="singular">
-                <input type="radio" id="Three" name="selectanswer"/>
-                <label for='Three'>Option 3</label> <br />
-                </div>
-                <div className="singular">
-                <input type="radio" id="Four" name="selectanswer"/>
-                <label for='Four'>Option 4</label> <br />
-                </div>
-                <div className="singular">
-                <input type="radio" id="Five" name="selectanswer"/>
-                <label for='Five'>Option 5</label> <br />
-                </div>
+           {currentQuestion.options.map((option, index) => {
+             return (<div className="singular" key={index}>
+                <input type="radio" 
+                index={index}
+                onChange={handleRadioChange}
+                value={option}
+                id={option}
+                name="selectanswer"
+                disabled={isSubmitted}
+                checked={selectedAnswer === option}
+                />
+                <label for={option} className={isSubmitted ? option === currentQuestion.correct ? 'correct-answer' : option === selectedAnswer ? 'wrong-answer' : '' : ''}>
+                    {option}
+                </label>
+            </div>)
+           })}
             </div>
-            <button>Submit Answer</button>
+            <button onClick={isSubmitted ? nextQuestion : handleSubmit}>{isSubmitted ? 'Next Question' : 'Submit Answer'}</button>
         </div>
     )
 }
